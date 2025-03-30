9acf7280e0f66bab4662d8158b2653ef23be384b
Wender Machado <devwendermachado@gmail.com>
2025-03-29 22:01:59 -0300

    Procurano Palindromo

--- index.js
+++ index.js
@@ -1,39 +1,15 @@
-function menu(){
-  return prompt(`Escolha uma das opções:
-    1-Nada
-    2-Nada
-    3-Nada
-    4-Nada
-    5-Encerrar`)
+let palavra = prompt("Qual a palavra?");
+let inversalWorld = ''
+
+function buscandoPalindrom(palavra) {
+  for (let i = palavra.length - 1; i >= 0; i--) {
+    inversalWorld += palavra[i]
+  }
+  if (palavra === inversalWorld) {
+    console.log(`"${palavra}" é um palíndromo!`)
+  } else {
+    console.log(`"${palavra}" não é um palíndromo.`)
+  }
 }
 
-function executar(){
-  let opcao =''
-  do {
-    opcao = menu()
-    switch (opcao) {
-    case "1":
-        alert(`invalido`)
-        break;
-    case "2":
-      alert(`invalidsddo`)
-      break;
-    case "3":
-      alert(`iasdlido`)
-      break;
-    case "4":
-      alert(`invalido`)
-      break;
-    case "5":
-      alert(`encerando`)
-      break;
-      default:
-        alert(`valide novamente`)
-        break;
-    }
-
-
-  } while (opcao !== "5");
-}
-
-executar()
+buscandoPalindrom(palavra)
