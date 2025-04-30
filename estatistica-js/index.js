ca409f7d1f31b947466048be4ff3e9c44cf3f034
Wender Machado <devwendermachado@gmail.com>
2025-04-30 10:00:53 -0300

    adiciona function de moda e mediana

--- estatistica-js/index.js
+++ estatistica-js/index.js
@@ -11,8 +11,8 @@ console.log(simpleAverage(2, 6, 3, 7, ))
 
 function weightedAverage(...nums){
 
-  const sum = nums.reduce((accum, {number, weight})=> accum + (number * weight), 0)
-  const sumWeight = nums.reduce((accum, entry) => accum + entry.weight, 0 )
+  const sum = nums.reduce((accum, {number, weight})=> accum + (number * (weight ?? 1)), 0)
+  const sumWeight = nums.reduce((accum, entry) => accum + (entry.weight ?? 1), 0 )
 
   return sum / sumWeight
 }
@@ -22,3 +22,26 @@ console.log(weightedAverage(
   { number: 7, weight: 1 },
   { number: 10, weight: 2 }
 ))
+
+function calculateMedian(...nums){
+ const orderedNumbers = [...nums].sort((a, b)=> a - b)
+const midle = Math.floor(orderedNumbers.length / 2)
+if(orderedNumbers.length % 2 !== 0 ){
+  return  orderedNumbers[midle]
+}
+const firstMedian = orderedNumbers[midle -1]
+const secondeMedian = orderedNumbers[midle]
+return simpleAverage(firstMedian, secondeMedian)
+}
+
+console.log(`Mediana: ${calculateMedian(2, 5, 99, 4, 42, 7)}`)
+console.log(`Mediana: ${calculateMedian(15, 14, 8, 7, 3)}`)
+
+function mode(...nums){
+  const quantities =  nums.map(num => [
+    num,
+    nums.filter(n => num === n).length
+  ])
+  quantities.sort((a,b) => b[1] - a[1])
+  return quantities [0][0]
+}
