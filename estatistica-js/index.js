5574e3f1ed9aea37dc14c2204d34d3a6a2c8d83d
Wender Machado <devwendermachado@gmail.com>
2025-04-30 08:50:33 -0300

    Treino com função de media ponderada

--- estatistica-js/index.js
+++ estatistica-js/index.js
@@ -6,4 +6,19 @@ function simpleAverage(...nums) {
   const average = sum / nums.length
   return average
 }
-console.log(simpleAverage(2, 6, 3, 7, 4))
+console.log(simpleAverage(2, 6, 3, 7, ))
+
+
+function weightedAverage(...nums){
+
+  const sum = nums.reduce((accum, {number, weight})=> accum + (number * weight), 0)
+  const sumWeight = nums.reduce((accum, entry) => accum + entry.weight, 0 )
+
+  return sum / sumWeight
+}
+
+console.log(weightedAverage(
+  { number: 9, weight: 3 },
+  { number: 7, weight: 1 },
+  { number: 10, weight: 2 }
+))
