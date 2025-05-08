43071273459a60d72dfb2e77880367a765063d2b
Wender Machado <devwendermachado@gmail.com>
2025-05-08 09:50:04 -0300

    criação de personagens com POO

---
+++ GameCaracheters/Character.js
@@ -0,0 +1,13 @@
+class Character{
+  constructor(name, lifePoints, atack, defense){
+    this.name = name
+    this.lifePoints = lifePoints
+    this.atack = atack
+    this.defense = defense
+  }
+  atackMove(persona){
+    persona.lifePoints -= this.atack - this.defense
+    return persona
+  }
+}
+
