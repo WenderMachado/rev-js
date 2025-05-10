5f094bffc8cb5878bb55ba997e976d45bd30b775
Wender Machado <devwendermachado@gmail.com>
2025-05-09 23:20:10 -0300

    Adiciona classe de pedidios Order e Classe poster

---
+++ JSBOOK/classes/Book.js
@@ -0,0 +1,11 @@
+const Product = require("./Product");
+
+module.exports = class Book extends Product{
+  constructor(title, synopsis, genre, page, author, description, price, inStock = 0){
+    super(`Livro: ${title}`, description, price, inStock)
+    this.synopsis = synopsis
+    this.genre = genre
+    this.page = page 
+    this.author = author
+  }
+}
