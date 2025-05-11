2993a60702dc74d8072a031928930a7f01a033fa
Wender Machado <devwendermachado@gmail.com>
2025-05-11 10:56:41 -0300

    adiciona banco de dados improvisado -Classes Restantes

--- JSBOOK/entities/Order.js
+++ JSBOOK/entities/Order.js
@@ -3,7 +3,7 @@ module.exports = class Order {
   #items
   #user
   constructor(items, user){
-    items.array.forEach(({product, quantity}) => {
+    items.forEach(({product, quantity}) => {
       if(quantity  > product.inStock){
         throw new Error(`Quantidade insuficiente em estoque`)
       }
@@ -13,7 +13,7 @@ module.exports = class Order {
     this.#total = items.reduce((sum, {product, quantity}) => sum + (product.price * quantity), 0)
   }
 
-  get (){
+  get data(){
     return {
       item: this.#items,
       user: this.#user,
