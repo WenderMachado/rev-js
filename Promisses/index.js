72aceee7fdadb3e2b8f4ecc6c576700db60edb2e
Wender Machado <devwendermachado@gmail.com>
2025-05-17 09:47:38 -0300

    revisão uso de prmisses

---
+++ Promisses/index.js
@@ -0,0 +1,25 @@
+function imc(weight, height){
+  return new Promise((resolve, reject) => {
+    if(typeof weight === "number" && typeof height === "number"){
+      resolve(weight / (height * height))
+    } else{
+      reject(`operação inválda`)
+    }
+  })
+}
+function showImc(weight, height) {
+  imc(weight, height)
+  .then((result) => {
+    console.log(`O resultado do IMC foi de ${result}.`)
+
+    if (result < 18.5) console.log('Situação: MAGREZA')
+    else if (result < 25) console.log('Situação: NORMAL')
+    else if (result < 30) console.log('Situação: SOBREPESO')
+    else if (result < 40) console.log('Situação: OBESIDADE')
+    else console.log('Situação: OBESIDADE GRAVE')
+  }).catch((err) => {
+    console.log(err)
+  })
+    console.log(`Calculando o IMC para peso ${weight} e altura ${height}...`)
+}
+showImc(67, 1.65)
