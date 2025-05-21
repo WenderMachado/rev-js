9405e9cc205a29d5ab3c914f3e0b27548da753a0
Wender Machado <devwendermachado@gmail.com>
2025-05-21 10:27:10 -0300

    refatorando promisses

--- Promisses/index.js
+++ Promisses/index.js
@@ -1,25 +1,32 @@
-function imc(weight, height){
-  return new Promise((resolve, reject) => {
-    if(typeof weight === "number" && typeof height === "number"){
-      resolve(weight / (height * height))
-    } else{
-      reject(`operação inválda`)
-    }
-  })
-}
-function showImc(weight, height) {
-  imc(weight, height)
-  .then((result) => {
-    console.log(`O resultado do IMC foi de ${result}.`)
 
+async function imc(weight, height) {
+    if(typeof weight === "number" && typeof height === "number"){
+      return(weight / (height * height))
+    } else{
+      throw new Error('deu erro patraO')
+    }
+}
+
+async function showImc(weight, height) {
+  try {
+    console.log(`Calculando o IMC para peso ${weight} e altura ${height}...`)
+
+    const result = await imc(weight, height)
+    
+    console.log(`O resultado do IMC foi de ${result}.`)
+  
     if (result < 18.5) console.log('Situação: MAGREZA')
     else if (result < 25) console.log('Situação: NORMAL')
     else if (result < 30) console.log('Situação: SOBREPESO')
     else if (result < 40) console.log('Situação: OBESIDADE')
     else console.log('Situação: OBESIDADE GRAVE')
-  }).catch((err) => {
-    console.log(err)
-  })
-    console.log(`Calculando o IMC para peso ${weight} e altura ${height}...`)
+  } catch (error) {
+    console.log(error)
+  }
 }
-showImc(67, 1.65)
+
+showImc(71, 1.74)
+showImc(48, 1.60)
+showImc(71, "texto")
+showImc(82, 1.72)
+showImc(120, 1.80)
