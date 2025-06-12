83de29c27b97b3ccad17aa21d40cc7b7ad5f56c2
Wender Machado <devwendermachado@gmail.com>
2025-06-12 07:36:01 -0300

    trata as excessões de clic, enter e backspace

--- calc-js/index.js
+++ calc-js/index.js
@@ -18,4 +18,10 @@ input.addEventListener("keydown", (ev)=>{
     input.value += ev.key
     return
   }
+  if(ev.key === 'Backspace'){
+    input.value =  input.value.slice(0, -1)
+  }
+  if(ev.key === 'Enter'){
+    calculate()
+  }
 })
